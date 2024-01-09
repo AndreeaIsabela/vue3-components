cc48342911d6473dadb8be98b2aa22998e8396f4
Isabela Cioarec <bala.andreea@yahoo.com>
2024-01-09 23:24:54 +0200

    [main] Add Pagination view.

---
+++ src/views/PaginationView.vue
@@ -0,0 +1,27 @@
+<template lang='pug'>
+.row.d-flex
+  .col-12.my-auto
+    Pagination(
+      :totalPages='10' 
+      :maxVisibleButtons='3'
+      :currentPage='currentPage'
+      @page-changed='handlePageChange'
+    )
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Pagination from '@/components/Pagination.vue'
+
+const currentPage = ref(2);
+
+const handlePageChange = (newPage) => {
+  // Fetch some data
+  currentPage.value = newPage;
+}
+</script>
+
+<style lang='stylus'>
+.row
+  height: 100%
+</style>
