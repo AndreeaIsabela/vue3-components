9d756a2c006de042d897747703ad517cd9b974a8
Isabela Cioarec <bala.andreea@yahoo.com>
2024-02-22 20:08:42 +0200

    [main] Add pagination component & pagination story.

---
+++ src/Pagination.story.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+import Pagination from './Pagination.vue';
+
+const currentPage = ref(2);
+
+const handlePageChange = (newPage) => {
+  // Fetch some data
+  currentPage.value = newPage;
+}
+
+</script>
+
+<template lang='pug'>
+story(title='Pagination')
+  pagination(
+    :totalPages='10'
+    :maxVisibleButtons='3'
+    :currentPage='currentPage'
+    @page-changed='handlePageChange'
+  )
+</template>
